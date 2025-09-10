aa012f897b7b5a3540084e22a121ad0992ef9307
Juan Martín Grado <114537878+Juanmgrado@users.noreply.github.com>
2025-09-10 12:43:33 +0200

    fix: the gotten number its not showed in an alert anymore

--- trivia.js
+++ trivia.js
@@ -1,12 +1,13 @@
 const rollDiceButton = document.querySelector("#btn-dado")
+const numberContainer = document.querySelector("#numero-obtenido")
 
 const rollDice = () => {
+    
     const MIN_DICE_NUMBER = 1
     const MAX_DICE_NUMBER = 6
     let randomRollDiceNumber = Math.floor(Math.random() * MAX_DICE_NUMBER) + MIN_DICE_NUMBER
 
-    alert (`El número es el:  ${randomRollDiceNumber}`)
-    return randomRollDiceNumber
+    numberContainer.innerHTML = `El número obtenido es el: ${randomRollDiceNumber}`
     
 }
 
