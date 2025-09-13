5e53df70ef90707a01f6cac7769f972e06358d8a
LmartCid <lmartcid@gmail.com>
2025-09-13 18:08:34 +0200

    questions and answers showed

---
+++ gameQuestions.js
@@ -0,0 +1,52 @@
+
+
+const questions = [
+    {
+     id:1,    
+     topic:"Physics", 
+     statement:`Un cuerpo sumergido en un fluido experimenta una fuerza de empuje hacia arriba igual al volumen de liquido que desaloja. ¿Cómo se llama este principio de la física?:`,
+    options: ["Principio de incertidumbre de Heisenberg", "Principio de Bernoulli", "Principio de Arquímedes", "Principio de Newton"],
+    correct:"Principio de Arquímedes"
+    }, 
+
+    { 
+     id:2, 
+     topic:"Software development", 
+     statement:`¿Que tipo de programación se emplea para desarrollar un programa que es capaz de realizar mas de una tarea al mismo tiempo?:`,
+   
+     options: ["Programación estructurada", "Programación secuencial", "Programación modular", "Programación multihilo"], 
+     correct:"Programación multihilo"  
+    }, 
+
+    {
+     id:3, 
+     topic: "Science Nature", 
+     statement:"¿Cuál es la molécula encargada de almacenar y transmitir la información genética en los seres vivos?:", 
+     options:["ARN", "Ácido Desoxirribonucleico", "Bilirrubina", "Glucosa"], 
+     correct:"Ácido Desoxirribonucleico" 
+    }, 
+
+    {
+     id:4, 
+     topic:"Sport", 
+     statement:`¿Que equipo de fútbol profesional de la primera división española consiguió ganar la liga en la temporada 2013-2014?:`,
+     options:["Real Madrid", "FC Barcelona", "Atlético de Madrid", "Real Betis balonpié"], 
+     correct: "Atlético de Madrid"
+    }, 
+
+    {
+     id:5, 
+     topic:"TV show and affairs", 
+     statement:"¿Qué cantante española confirmo en 2023 el fin de su compromiso con Rauw Alejandro?", 
+     options:["Aitana", "Rosalía", "Lola Índigo", "Bad Gyal"], 
+     correct:"Rosalía"
+    }, 
+
+    {
+     id:6, 
+     topic:"famous people", 
+     statement:"¿ Quién es Bad Bunny?",  
+     options:["Un cantante", "Un científico", "Un escritor", "Un programador muy bueno"], 
+     correct:"Un cantante" 
+    }
+]
